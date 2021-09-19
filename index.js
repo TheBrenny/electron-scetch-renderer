eb113f817b086dc072318c066a0da02d442e9abf
TheBrenny <iam+dev@justbrenny.me>
2021-09-20 01:33:36 +1000

    0130 am

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pathLib = require("path");
-const fs =require("fs");
+const fs = require("fs");
 const scetch = require("scetch");
 const mime = require('mime');
 const URL = require("url").URL;
@@ -19,47 +19,49 @@ function setup(electron, scetchOptions) {
 
     const emitter = new EventEmitter();
     electron.protocol.interceptBufferProtocol("file", (request, callback) => {
-        let url = new URL(request.url);
-        let path = decodeURIComponent(url.pathname);
-        if(process.platform === "win32" && !url.host.trim()) path = path.substring(1);
+        (async () => {
+            let url = new URL(request.url);
+            let path = decodeURIComponent(url.pathname);
+            if(process.platform === "win32" && !url.host.trim()) path = path.substring(1);
 
-        let vars = {};
-        let searchParams = url.searchParams;
-        for(let [key, val] of searchParams) {
-            key = key.split(".");
-            let obj = vars;
-            for(let i = 0; i < key.length; i++) {
-                let k = key[i];
-                obj[k] = (i === key.length - 1 ? val : obj[k]) ?? {};
-                obj = obj[k];
-            }
-        }
-
-        try {
-            let content = fs.readFileSync(path);
-            let ext = pathLib.extname(path);
-            let data = {data: content, mimeType: mime.getType(ext)};
-
-            if(ext === '.sce') {
-                let compiled = scetch.engine(path, vars);
-                data = {data: Buffer.from(compiled), mimeType: htmlMime};
+            let vars = {};
+            let searchParams = url.searchParams;
+            for(let [key, val] of searchParams) {
+                key = key.split(".");
+                let obj = vars;
+                for(let i = 0; i < key.length; i++) {
+                    let k = key[i];
+                    obj[k] = (i === key.length - 1 ? val : obj[k]) ?? {};
+                    obj = obj[k];
+                }
             }
 
-            emitter.emit("data", data);
-            return callback(data);
-        } catch(err) {
-            let errorData;
-            if(err.code === 'ENOENT') {
-                errorData = -6;
-            } else if(typeof err.code === 'number') {
-                errorData = -2;
-            } else {
-                errorData = {data: Buffer.from(`<pre style="tab-size:1">${err}</pre>`), mimeType: htmlMime};
-            }
+            try {
+                let content = fs.readFileSync(path);
+                let ext = pathLib.extname(path);
+                let data = {data: content, mimeType: mime.getType(ext)};
 
-            emitter.emit("error", err);
-            return callback(errorData);
-        }
+                if(ext === '.sce') {
+                    let compiled = await scetch.engine(path, vars);
+                    data = {data: Buffer.from(compiled), mimeType: htmlMime};
+                }
+
+                emitter.emit("data", data);
+                return callback(data);
+            } catch(err) {
+                let errorData;
+                if(err.code === 'ENOENT') {
+                    errorData = -6;
+                } else if(typeof err.code === 'number') {
+                    errorData = -2;
+                } else {
+                    errorData = {data: Buffer.from(`<pre style="tab-size:1">${err}</pre>`), mimeType: htmlMime};
+                }
+
+                emitter.emit("error", err);
+                return callback(errorData);
+            }
+        })();
     });
     return emitter;
 }
